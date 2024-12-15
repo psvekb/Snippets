5f6135158b167e56c9eddc52e3218af9098f2ede
Sergej pakhomov <psv.ekb@gmail.com>
2024-12-15 20:30:13 +0300

    Snippets step-13

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Snippet
-
+from MainApp.forms import SnippetForm
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -16,3 +16,28 @@ def add_snippet_page(request):
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов', 'snippets' : Snippet.objects.all(), 'snippets_count':Snippet.objects.count()}
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet(request, id):
+    try:
+        snippet = Snippet.objects.get(id = id)
+        
+        # colors = item.colors.all()
+        # print(f'{colors=}')
+    except:
+        return Http404(f'Сниппет c {id = } не найден')
+    
+    print(snippet)    
+    context = {'pagename': 'Сниппет', 'snippet' : snippet}
+    return render(request, 'pages/snippet.html', context)
+
+def add_snippet_page(request):
+    form = SnippetForm()
+    return render(request, 'add_snippet.html', {'form': form})
+
+def create_snippet(request):
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("redirect_url")
+    return render(request,'add_snippet.html',{'form': form})    
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -6,5 +6,6 @@ from MainApp import views
 urlpatterns = [
     path('', views.index_page, name='index'),
     path('snippets/add', views.add_snippet_page, name='add_snippet'),
+    path('snippet/<int:id>', views.snippet, name='snippet'),
     path('snippets/list', views.snippets_page, name='list_snippets'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
